f43868aa7f8d3951313e4c0dc15eae884712c12b
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-03-08 18:25:38 -0500

    Create Object.js

---
+++ ObjectMaps/Object.js
@@ -0,0 +1,12 @@
+const blue = '#00f';
+const orange = '#f60';
+
+// variables - boxes; objects - file cabinets
+
+const colors = {yellow: '#ff0',blue,orange}
+console.log(colors);
+
+// objectName.key
+console.log(colors.yellow);
+
+
