e31e8968d1f43a63c8a66d1b377cee3794244b4f
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-03-08 22:25:12 -0500

    Update Object.js

--- ObjectMaps/Object.js
+++ ObjectMaps/Object.js
@@ -31,3 +31,56 @@ favouritePlaces.greeting()
 console.log(favouritePlaces.bar);
 console.log(favouritePlaces.cafe);
 console.log(favouritePlaces.restaurant);
+
+
+
+//Destructuring
+
+const user = {
+  name: "Reed",
+  username: "Reedbarger",
+  email: "reed@gmail.com",
+  details: {
+    title: "Programmer"  
+  }  
+};
+
+// const { title } = user.details
+// const { name, details: { title} } = user;
+
+function displayUserBio({ name, details: {title} }) 
+{
+  console.log(`${name} is a ${title}`); 
+}
+
+displayUserBio(user);
+
+
+// Challenge: The recommendations object contains a set of of nice places to visit in Brighton, UK,
+// organized by what you'd like to do (eat pancakes, drink coffee etc).
+
+// 1. Destructure the places to drink (coffee and beer) from recommendations
+// 2. Destructure the places to listen to music
+// 3. Write a function that takes the recommendations object as an argument and that
+//      a) Logs out the music venues in recommendations when invoked 
+//      b) Uses object descructuring to get the "traditional" and "jazz" keys from the argument
+
+const recommendations = {
+    pancakes: 'Nowhere Man',
+    riceBowls: 'Pompoko',
+    beer: 'The Craft Beer Co.',
+    coffee: 'Coffee Roasters',
+    small_plates: 'Venetian Plates',
+    music: { 
+        traditional: 'Fiddler\'s Elbow', 
+        jazz: 'The Paris House'
+    }
+}
+
+const {coffee, beer, music: {traditional, jazz}} = recommendations;
+
+function displayMusicPlaces({music:{traditional, jazz}}){
+    console.log(`Music: ${traditional}, ${jazz}`)
+}
+
+playMusic(recommendations);
