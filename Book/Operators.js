cfe70288c6392b6a234464263d1534e147bcdd8d
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-03-11 12:51:30 -0500

    Update Operators.js

--- Book/Operators.js
+++ Book/Operators.js
@@ -17,3 +17,26 @@ else if (login === 'Admin') {
 else {
     alert(`I don't know you`);
 }
+
+
+
+
+for (let index = 2; index <= 10; index++) {
+    if (index % 2 == 0) { alert(index) };
+}
+
+
+let i = 0;
+while (i < 3) {
+    alert(`number ${i}!`)
+    i++;
+}
+
+
+let num;
+
+do {
+  num = prompt("Enter a number greater than 100?", 0);
+} while (num <= 100 && num);
+
+
