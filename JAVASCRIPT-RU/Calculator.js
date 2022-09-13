b7e8c984a63dc994c7565e3dc9e47f1884ca6b6b
Nadia Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2022-09-13 14:11:26 -0400

    Update and rename Calculator.java to Calculator.js

--- JAVASCRIPT-RU/Calculator.js
+++ JAVASCRIPT-RU/Calculator.js
@@ -21,3 +21,23 @@ let resultSum = calculator.sum();
 console.log(resultSum);
 let resultMul = calculator.mul();
 console.log(resultMul);
+
+
+//==========
+function Calculator(){
+  this.read= function (){
+    this.number1 = +prompt('First number', 0);
+    this.number2 = +prompt('Second number', 0);
+  };
+   this.sum= function (){
+    return (this.number1 + this.number2);
+  };
+    this.mul= function (){
+      return (this.number1 * this.number2 )
+    };
+}
+
+let calculator1 =  new Calculator();
+calculator1.read();
+console.log("Sum:" + calculator1.sum());
+console.log("Mul:" + calculator1.mul());
